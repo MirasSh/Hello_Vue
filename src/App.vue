c0822d88639a9717481d5cf1c83385e2026db199
MirasSh <miras.anisa@gmail.com>
2023-06-03 21:16:25 +0600

    Helli world and Vue

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+import AboutView from '../src/views/AboutView.vue'
+
+const products = ref([
+  'Hello Vue',
+  'Бананы',
+  'Яблоки',
+  'Груша',
+  'Алма',
+  'Нан',
+  'Сүт',
+  'Қызанақ',
+
+])
+
+</script>
+<template>
+  <div>
+    <ul>
+      <AboutView />
+      <li v-for="product in products"
+      :key="product"
+      >
+        {{ product }}
+      </li>
+    </ul>
+  </div>
+</template>
+
---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1><b>Hello World and Vue</b></h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    color: red;
+  }
+}
+</style>
